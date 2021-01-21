d008b23de4165220f1c0b6b61631f2b4e0e230f6
laurendaines <laurendaines@gmail.com>
2021-01-20 20:52:56 -0700

    done

--- wwwroot/js/grade.js
+++ wwwroot/js/grade.js
@@ -1,9 +1,47 @@
 ﻿function calculator() {
-    var grade1 = parseFloat(document.getElementById('homework').value * 0.25);
-    var grade2 = parseFloat(document.getElementById('labs').value * 0.20);
-    var grade3 = parseFloat(document.getElementById('midterm').value * 0.25);
-    var grade4 = parseFloat(document.getElementById('finals').value * 0.30);
-    var total = grade1 + grade2 + grade3 + grade4;
+    var grade1 = parseFloat(document.getElementById('assignments').value * 0.50);
+    var grade2 = parseFloat(document.getElementById('groupproject').value * 0.10);
+    var grade3 = parseFloat(document.getElementById('quizzes').value * 0.10);
+    var grade4 = parseFloat(document.getElementById('exams').value * 0.30);
+    var grade5 = parseFloat(document.getElementById('intex').value * 0.10);
+    var total = grade1 + grade2 + grade3 + grade4 + grade5;
 
-    var display = document.getElementById('outputDiv').innerHTML = 'Your Final Grade Is: ' + total +'%';
+    if (total >= 94) {
+        letter = "Your letter grade is an A!";
+    }
+    if (total <= 93 && total >= 90) {
+        letter = "Your letter grade is a A-!";
+    }
+    if (total <= 89 && total >= 87) {
+        letter = "Your letter grade is a B+";
+    }
+    if (total <= 86 && total >= 84) {
+        letter = "Your letter grade is a B!";
+    }
+    if (total <= 83 && total >= 80) {
+        letter = "Your letter grade is a B-!";
+    }
+    if (total <= 79 && total >= 77) {
+        letter = "Your letter grade is a C+!";
+    }
+    if (total <= 76 && total >= 74) {
+        letter = "Your letter grade is a C!";
+    }
+    if (total <= 73 && total >= 70) {
+        letter = "Your letter grade is a C-!";
+    }
+    if (total <= 69 && total >= 67) {
+        letter = "Your letter grade is a D+!";
+    }
+    if (total <= 66 && total >= 64) {
+        letter = "Your letter grade is a D!";
+    }
+    if (total <= 63 && total >= 60) {
+        letter = "Your letter grade is a D-!";
+    }
+    if (total < 60) {
+        letter = "Your letter grade is an E!";
+    }
+
+    var display = document.getElementById('outputDiv').innerHTML = "Your Final Grade Is: " + total + "% \n" + letter;
 }
